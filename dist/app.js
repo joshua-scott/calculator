4897d3508ca0fa496ffffe4985c8225f2705a54a
Joshua Scott <jscott313@gmail.com>
2017-09-13 23:29:11 +0300

    Some basic click handling done

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,25 @@
 'use strict';
 
 const buttons = document.querySelectorAll('.buttons button');
-buttons.forEach(btn => btn.textContent = btn.dataset.num || btn.dataset.op);
+const topScreen = document.querySelector('.previous-input');
+const bottomScreen = document.querySelector('.current-input');
+let calculation = '';
+
+function handleClick() {
+  const clicked = this.dataset.content;
+  if (clicked === 'C') {
+    clearAll();
+  } else if (clicked === '&#x2190;') {
+    calculation = calculation.slice(0, -1);
+  } else if (clicked === '=') {
+    calculate();
+  } else {
+    calculation += clicked;
+  }
+}
+
+buttons.forEach(btn => {
+  btn.textContent = btn.dataset.content;
+  btn.addEventListener('click', handleClick);
+});
 //# sourceMappingURL=app.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,22 @@
 const buttons = document.querySelectorAll('.buttons button');
-buttons.forEach(btn => btn.textContent = btn.dataset.num || btn.dataset.op);
+const topScreen = document.querySelector('.previous-input')
+const bottomScreen = document.querySelector('.current-input')
+let calculation = '';
+
+function handleClick() {
+  const clicked = this.dataset.content;
+  if (clicked === 'C') {
+    clearAll();
+  } else if (clicked === '&#x2190;') {
+    calculation = calculation.slice(0, -1);
+  } else if (clicked === '=') {
+    calculate();
+  } else {
+    calculation += clicked;
+  }
+}
+
+buttons.forEach(btn => {
+  btn.textContent = btn.dataset.content;
+  btn.addEventListener('click', handleClick);
+});
